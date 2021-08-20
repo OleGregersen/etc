35751e7381d61381975328e212b4c3e930302ddb
OleGregersen <84504834+OleGregersen@users.noreply.github.com>
2021-08-20 12:21:43 +0200

    Create Word Order.py

---
+++ HR/Python/Collections/Word Order.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import collections
+
+n = int(input())
+d = collections.OrderedDict()
+for i in range(n):
+    word = input()
+    if word in d:
+        d[word] += 1
+    else:
+        d[word] = 1
+print(len(d));
+for k,v in d.items():
+    print(v, end = " ");
