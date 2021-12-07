ac007ca440eac5a599acfa006735b7e12d7522b9
OleGregersen <84504834+OleGregersen@users.noreply.github.com>
2021-12-07 10:46:51 +0100

    Add files via upload

---
+++ HR/C/Introduction/Playing_With_Characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define MAX_LEN 40
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char ch;
+    char s[MAX_LEN];
+    char sen[MAX_LEN];
+    scanf("%c",&ch);
+    scanf("%s",&s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n%s\n%s",ch,s,sen); 
+    return 0;
+}
